95cc2ca79e7a259f70f06ef65ad49e1d33b51ae2
Zcommand <zumoshimo25@gmail.com>
2026-09-09 20:08:10 +0800

    Complete Campus Companion welcome screen

--- CampusCompanion/ViewController.swift
+++ CampusCompanion/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "Campus Companion"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStartedButtonTapped(_ sender: UIButton) {
+        subtitleLabel.text = "Let's get started!"
+    }
+    
 }
 
